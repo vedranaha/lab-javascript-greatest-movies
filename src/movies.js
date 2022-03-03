8d3c964130d1df479c04504f11566fe1f49bd8ba
Vedrana <vedranaha@gmail.com>
2022-03-04 00:53:38 +0100

    until 3rd iteration

--- src/movies.js
+++ src/movies.js
@@ -1,10 +1,27 @@
+const movies = require('./data');
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+
+function getAllDirectors() {
+  const directors = movies.map((movie) => movie.director);
+  const directorsClean = directors.filter(
+    (element, index) => directors.indexOf(element) === index
+  );
+  return directorsClean.sort();
+}
+console.log(getAllDirectors());
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies() {
+  const spielbergDrama = movies.filter(
+    (movie) =>
+      movie.director === 'Steven Spielberg' && movie.genre.includes('Drama')
+  );
+  return spielbergDrama;
+}
+console.log(howManyMovies());
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage() {}
@@ -24,8 +41,6 @@ function turnHoursToMinutes() {}
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg() {}
 
-
-
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
@@ -37,6 +52,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     orderAlphabetically,
     turnHoursToMinutes,
-    bestYearAvg,
+    bestYearAvg
   };
 }
